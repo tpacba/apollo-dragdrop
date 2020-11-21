35455c22d2db37c885625342fafac8249005301b
Tristan Pacba <tpacba@live.com>
2020-11-21 08:47:31 -0800

    was able to upload an image using altair, with file variable

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,27 @@
+const shortid = require('shortid');
+const { createWriteStream, mkdir } = require('fs');
+
+const File = require('../models/fileModel');
+
+async function storeUpload({ stream, filename, mimetype }) {
+    const id = shortid.generate();
+    const path = `images/${id}-${filename}`;
+
+    return new Promise((resolve, reject) => {
+        stream
+            .pipe(createWriteStream(path))
+            .on("finish", () => resolve({ id, path, filename, mimetype }))
+            .on("error", reject)
+    });
+};
+
+async function processUpload(upload) {
+    const { createReadStream, filename, mimetype } = await upload;
+    const stream = createReadStream();
+    const file = await storeUpload({ stream, filename, mimetype });
+    return file;
+};
+
 const resolvers = {
     Query: {
         async sayHello() {
@@ -6,6 +30,17 @@ const resolvers = {
             } catch (error) {
                 throw new Error(error)
             }
+        }
+    },
+    Mutation: {
+        async uploadFile(_, { file }) {
+            mkdir("images", { recursive: true }, (error) => {
+                if (error) {
+                    throw error;
+                }
+            })
+            const upload = await processUpload(file);
+            return upload;
         }
     }
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,8 +1,18 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+    type File {
+        id: ID!
+        filename: String!
+        mimetype: String!
+        path: String!
+    }
     type Query {
         sayHello: String!
+        readFiles: [File!]
+    }
+    type Mutation {
+        uploadFile(file: Upload!): File
     }
 `;
 
