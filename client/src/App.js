403d3b5982f9f9911c9583bfb9a51e9bc90b5cbe
Tristan Pacba <tpacba@live.com>
2020-11-25 18:18:34 -0700

    setting up Login with useMutation LOGIN_USER

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import { AuthProvider } from './utils/AuthProvider';
+import { AuthProvider } from './utils/Auth';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import { gql, useMutation } from '@apollo/client';
 
-const Login = () => {
+import { AuthContext } from '../utils/Auth';
+
+const LOGIN_USER = gql`
+    mutation login(
+        $username: String! 
+        $password: String!
+    ) {
+        login(
+            username: $username 
+            password: $password
+        ) {
+            id
+            email
+            token
+            username
+            createdAt
+        }
+    }
+`
+
+const Login = (props) => {
+    const context = useContext(AuthContext);
+
+    const [values, setValues] = useState({username: "", password: ""});
+
+    const [errors, setErrors] = useState({});
+
+    const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+        update(_, result) {
+
+        },
+        onError(error) {
+
+        },
+        variables: values
+    });
+
+    const onChange = (event) => {
+        setValues({...values, [event.target.name]: event.target.value})
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        loginUser();
+    };
+
     return (
         <div>
             
         </div>
     );
 };
-
-Login.propTypes = {};
 
 export default Login;
